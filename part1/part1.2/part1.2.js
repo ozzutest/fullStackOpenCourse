593a669b618b89e3d533fd77eafd64ae6fbe259b
ozzutest <47740803+ozzutest@users.noreply.github.com>
2019-08-29 00:26:52 +0200

    Create part1.2.js

---
+++ part1/part1.2/part1.2.js
@@ -0,0 +1,3 @@
+//Completly dunno how to do this exercise - i don't understand clearly an instruction, also i don't know how to replace 
+//Content elements including parts and exercises to single keyword Part, if you could please send me an answer or just 
+//a tip to resolve this task, thank You!
